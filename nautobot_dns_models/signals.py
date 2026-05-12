36ae23a1caaf8dc8ec93d94730a0f30618134a0a
Noah Robin <noahr@nvidia.com>
2026-05-12 08:30:23 -0400

    ruff cleanups

--- nautobot_dns_models/signals.py
+++ nautobot_dns_models/signals.py
@@ -7,7 +7,7 @@ from nautobot_dns_models.models import CatalogZoneMembership
 
 
 @receiver(m2m_changed, sender=CatalogZoneMembership)
-def assign_member_node_labels_on_members_add(sender, instance, action, reverse, model, pk_set, **kwargs):   # pylint: disable=unused-argument
+def assign_member_node_labels_on_members_add(sender, instance, action, reverse, model, pk_set, **kwargs):  # pylint: disable=unused-argument
     """
     Ensure through-table rows get labels when created via M2M add/set operations.
 
@@ -15,7 +15,6 @@ def assign_member_node_labels_on_members_add(sender, instance, action, reverse, 
     fail uniqueness checks before post_add runs. Pre-create memberships with
     validated_save() in pre_add, then clear pk_set to skip default bulk insert.
     """
-
     if reverse or not pk_set:
         return
 
